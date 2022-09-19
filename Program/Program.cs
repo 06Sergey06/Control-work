25e42dfe37172c5adb5351c591fb124a5d1167b4
Zinovev Sergey <sergzi@yandex.ru>
2022-09-20 05:28:24 +1100

    s

---
+++ Program/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+string[] array = { "1234", "1567", "-2", "876" };
+var result = new string[array.Length];
+var realSize = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
